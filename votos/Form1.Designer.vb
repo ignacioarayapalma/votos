a4504b50f5aa9b3b0bb964e6d8e9e3176f5e7d5a
ignacioarayapalma <ignacio.araya1@mail.udp.cl>
2021-12-07 21:44:03 -0300

    Agregar archivos de proyecto.

---
+++ votos/Form1.Designer.vb
@@ -0,0 +1,378 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.TextBox11 = New System.Windows.Forms.TextBox()
+        Me.TextBox12 = New System.Windows.Forms.TextBox()
+        Me.TextBox13 = New System.Windows.Forms.TextBox()
+        Me.TextBox14 = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(36, 149)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(134, 27)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(394, 189)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(133, 27)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(394, 226)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(133, 27)
+        Me.TextBox3.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(39, 182)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(131, 41)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "GRABAR"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(380, 139)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(171, 37)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "VOTOS"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(38, 48)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(131, 144)
+        Me.ListBox1.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(281, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(237, 23)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Sistema de Control de Votos"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 16.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Label1.Location = New System.Drawing.Point(17, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(534, 38)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "ELECCIONES PRESIDENTENCIALES 2021"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(17, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(581, 73)
+        Me.Panel1.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(16, 16)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(169, 20)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Control votos emitidos"
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Controls.Add(Me.ListBox1)
+        Me.Panel2.Location = New System.Drawing.Point(1, 229)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(202, 209)
+        Me.Panel2.TabIndex = 10
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(220, 192)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(168, 20)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "1. GABRIEL BORIC FONT"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(224, 229)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(164, 20)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "2. JOSÉ ANTONIO KAST"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(237, 327)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(107, 20)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "3. Votos Nulos "
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(237, 361)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(130, 20)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "4. Votos en Blanco"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(237, 397)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(102, 20)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "Votación Final"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(394, 324)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox4.TabIndex = 16
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(393, 361)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox5.TabIndex = 17
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(393, 394)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox6.TabIndex = 18
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(234, 298)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(154, 20)
+        Me.Label9.TabIndex = 19
+        Me.Label9.Text = "Validamente emitidos"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(394, 291)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox7.TabIndex = 20
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(557, 141)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(248, 33)
+        Me.Button3.TabIndex = 21
+        Me.Button3.Text = "RESULTADOS PORCENTUALES %"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(620, 189)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox8.TabIndex = 22
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(620, 222)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox9.TabIndex = 23
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Location = New System.Drawing.Point(620, 291)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox10.TabIndex = 24
+        '
+        'TextBox11
+        '
+        Me.TextBox11.Location = New System.Drawing.Point(620, 324)
+        Me.TextBox11.Name = "TextBox11"
+        Me.TextBox11.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox11.TabIndex = 25
+        '
+        'TextBox12
+        '
+        Me.TextBox12.Location = New System.Drawing.Point(620, 361)
+        Me.TextBox12.Name = "TextBox12"
+        Me.TextBox12.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox12.TabIndex = 26
+        '
+        'TextBox13
+        '
+        Me.TextBox13.Location = New System.Drawing.Point(620, 394)
+        Me.TextBox13.Name = "TextBox13"
+        Me.TextBox13.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox13.TabIndex = 27
+        '
+        'TextBox14
+        '
+        Me.TextBox14.Location = New System.Drawing.Point(667, 41)
+        Me.TextBox14.Name = "TextBox14"
+        Me.TextBox14.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox14.TabIndex = 28
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(667, 12)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(115, 20)
+        Me.Label10.TabIndex = 29
+        Me.Label10.Text = "Ingrese ID Mesa"
+        '
+        'form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(866, 510)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.TextBox14)
+        Me.Controls.Add(Me.TextBox13)
+        Me.Controls.Add(Me.TextBox12)
+        Me.Controls.Add(Me.TextBox11)
+        Me.Controls.Add(Me.TextBox10)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents TextBox11 As TextBox
+    Friend WithEvents TextBox12 As TextBox
+    Friend WithEvents TextBox13 As TextBox
+    Friend WithEvents TextBox14 As TextBox
+    Friend WithEvents Label10 As Label
+End Class
---
+++ votos/Form1.vb
@@ -0,0 +1,86 @@
+﻿Public Class form1
+
+
+    Public a(999999), i As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        a(i) = TextBox1.Text
+        ListBox1.Items.Add(a(i))
+        i = i + 1
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim K As Integer
+        Dim boric, kast, total, porcentaje, emitidos, nulo, blanco, b2, k2, n1, bl1, em1, to1 As Integer
+        K = i - 1
+        For i = 0 To K
+            If a(i) = 1 Then
+                boric = boric + 1
+
+            End If
+
+            If a(i) = 2 Then
+                kast = kast + 1
+            End If
+            If a(i) = 3 Then
+                nulo = nulo + 1
+            End If
+            If a(i) = 4 Then
+                blanco = blanco + 1
+            End If
+
+            total = boric + kast + nulo + blanco
+            emitidos = boric + kast
+
+            b2 = (boric * 100.0 / emitidos)
+            k2 = (kast * 100.0 / emitidos)
+            n1 = (nulo * 100.0 / total)
+            bl1 = (blanco * 100.0 / total)
+            em1 = (emitidos * 100.0 / total)
+            to1 = (total * 100.0 / total)
+
+        Next
+        TextBox8.Text = b2.ToString("0.## '%'")
+        TextBox9.Text = k2.ToString("0.## '%'")
+        TextBox10.Text = em1.ToString("0.## '%'")
+        TextBox11.Text = n1.ToString("0.## '%'")
+        TextBox12.Text = bl1.ToString("0.## '%'")
+        TextBox13.Text = to1.ToString("0.## '%'")
+
+
+    End Sub
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim K As Integer
+        Dim boric, kast, nulo, blanco, total, emitidos As Integer
+        K = i - 1
+        For i = 0 To K
+            If a(i) = 1 Then
+                boric = boric + 1
+
+            End If
+
+            If a(i) = 2 Then
+                kast = kast + 1
+            End If
+            If a(i) = 3 Then
+                nulo = nulo + 1
+            End If
+            If a(i) = 4 Then
+                blanco = blanco + 1
+            End If
+
+            total = boric + kast + nulo + blanco
+            emitidos = boric + kast
+
+        Next
+        TextBox2.Text = boric
+        TextBox3.Text = kast
+        TextBox4.Text = nulo
+        TextBox5.Text = blanco
+        TextBox6.Text = total
+        TextBox7.Text = emitidos
+    End Sub
+End Class
